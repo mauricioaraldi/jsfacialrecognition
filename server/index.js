5e384dbbef092afe915d776934cea6f084cc4767
Mauricio Araldi <araldi.mauricio@gmail.com>
2020-02-12 23:44:17 +0100

    Adding face recognition in the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import Koa from 'koa';
 import koaStatic from 'koa-static';
+import koaBodyParser from 'koa-bodyparser';
+import fs from 'fs';
+import * as canvas from 'canvas';
+import * as faceapi from 'face-api.js';
+import '@tensorflow/tfjs-node';
+
+const { Canvas, Image, ImageData } = canvas;
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
 const app = new Koa();
 
@@ -7,5 +15,43 @@ app.use(koaStatic('./pages'));
 app.use(koaStatic('./static'));
 app.use(koaStatic('./models'));
 app.use(koaStatic('./users_photos'));
+app.use(koaBodyParser());
+
+app.use(({request, res, url}) => {
+  if (url === '/recognize') {
+    const image = request.body.image.replace(/^data:image\/png;base64,/, "");
+    const filePath = `${__dirname}/../users_photos/example.png`;
+    const savedImage = fs.readFileSync(filePath, 'base64');
+
+    res.statusCode = 200;
+    res.write(JSON.stringify({user: false}));
+    res.end();
+  } else if (url === '/newuser') {
+    const name = request.body.name.replace(/ /g, '');
+    const image = request.body.image.replace(/^data:image\/png;base64,/, "");
+    const filePath = `${__dirname}/../users_photos/${name}.png`;
+    let isFileSaved = false;
+
+    fs.writeFileSync(filePath, image,
+      {
+        encoding: 'base64',
+        flag: 'w'
+      }
+    );
+
+    try {
+      fs.accessSync(filePath);
+      isFileSaved = true;
+    } catch (e) {
+      isFileSaved = false;
+    }
+
+    res.statusCode = 200;
+    res.write(JSON.stringify({
+      user: isFileSaved ? request.body.name : false
+    }));
+    res.end();
+  }
+});
 
 app.listen(3000);
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,71 +1,130 @@
-const labeledFaceDescriptors = async () => {
-  const imgUrl = '/photo.png';
-  const img = await faceapi.fetchImage(imgUrl);
-  const personName = 'It\'s you';
+const CANVAS_HEIGHT = 600;
+const CANVAS_WIDTH = 800;
+const MAX_DESCRIPTOR_DISTANCE = 0.6;
+const STATUS_ERROR = 'error';
+const STATUS_LOADING = 'loading';
+const STATUS_RECOGNIZING = 'recognizing';
+const STATUS_NOT_RECOGNIZED = 'notRecognized';
+const STATUS_RECOGNIZED = 'recognized';
+const STATUS_USER_CREATED = 'userCreated';
+const STATUS_USER_NOT_CREATED = 'userNotCreated';
 
-  const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+const video = document.createElement('video');
+video.autoplay = true;
 
-  if (!fullFaceDescription) {
-    throw new Error(`no faces detected for ${personName}`);
-  }
+let videoToCanvasRenderer;
 
-  const faceDescriptors = [fullFaceDescription.descriptor];
-  return new faceapi.LabeledFaceDescriptors(personName, faceDescriptors);
-};
+/**
+ * Changes the current page of the app
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ * 
+ * @param {String} page The page to navigate to
+ */
+function changePage(page) {
+  const pages = document.querySelectorAll('main[data-page]');
 
-async function renderDetection(video, canvas, context, frameRate,
-  maxDescriptorDistance, faceDescriptors) {
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  pages.forEach((element, index) => {
+    if (element.dataset.page === page) {
+      return element.classList.remove('hidden');
+    }
 
-  if (faceDescriptors) {
-    const fullFaceDescriptions = await faceapi.detectAllFaces(canvas)
-      .withFaceLandmarks().withFaceDescriptors();
-    const faceMatcher = new faceapi.FaceMatcher(faceDescriptors, maxDescriptorDistance);
-    const results = fullFaceDescriptions.map((fd) => faceMatcher.findBestMatch(fd.descriptor));
-
-    results.forEach((bestMatch, i) => {
-      const { box } = fullFaceDescriptions[i].detection;
-      const text = bestMatch.toString();
-      const drawBox = new faceapi.draw.DrawBox(box, { label: text });
-      drawBox.draw(canvas);
-    });
-  }
-
-  setTimeout(renderDetection, 1 / frameRate, video, canvas, context, frameRate,
-    maxDescriptorDistance, faceDescriptors);
+    element.classList.add('hidden');
+  });
 }
 
-window.onload = async () => {
-  const video = document.querySelector('video');
-  const canvas = document.querySelector('canvas');
-  const ctx = canvas.getContext('2d');
-  const messageEl = document.querySelector('p');
-  const maxDescriptorDistance = 0.6;
-  let faceDescriptors;
+/**
+ * Set the status of the application
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ * 
+ * @param {String} status Current status of the application
+ * @param {String} text Status message to be displayed
+ */
+function setAppStatus(status, text) {
+  const messageEl = document.querySelector('#status');
 
+  messageEl.setAttribute('data-status', status);
+  messageEl.innerHTML = text;
+}
+
+/**
+ * Get the status of the application
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ */
+function getAppStatus() {
+  return document.querySelector('#status').dataset.status;
+}
+
+/**
+ * Loads the face models into the app
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ */
+async function loadFaceModels() {
   await faceapi.loadSsdMobilenetv1Model();
   await faceapi.loadFaceLandmarkModel();
   await faceapi.loadFaceRecognitionModel();
+}
 
-  try {
-    faceDescriptors = await labeledFaceDescriptors();
-  } catch (e) {
-    messageEl.setAttribute('data-status', 'error');
-    messageEl.innerHTML = e.message.concat(`<br /><br /> If this
-      is an error about not finding the photo.png file, save the
-      image from canvas (with the correct name) inside the users_photos
-      folder and then reload your page`);
-  }
+/**
+ * Renders the canvas with the picture of the user
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ * 
+ * @param {HTMLVideoElement} video The video element used to make
+ * the canvas image
+ * @param {Canvas2DContext} context The context of the canvas
+ * to render images
+ * @param {frameRate} frameRate The rate at which the canvas updates
+ */
+async function renderDetection(video, context, frameRate) {
+  context.drawImage(video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  setTimeout(renderDetection, 1 / frameRate, video, context, frameRate);
+}
 
-  canvas.width = 800;
-  canvas.height = 600;
+/**
+ * Sends a face recognition request to the server
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ * 
+ * @param {String} imageData The base64 data of the image
+ * @return {String} Name of recognized user
+ */
+async function sendRecognitionRequest(imageData) {
+  return new Promise((resolve, reject) => {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
 
-  renderDetection(video, canvas, ctx, 1, maxDescriptorDistance, faceDescriptors);
+    fetch('/recognize', {
+      headers,
+      body: JSON.stringify({
+        image: imageData
+      }),
+      method: 'POST'
+    })
+      .then(data => data.json())
+      .then(response => {
+        resolve(response.user);
+      })
+      .catch(error => reject(error));
+  });
+}
 
-  if (messageEl.getAttribute('data-status') === 'loading') {
-    messageEl.textContent = '';
-  }
-
+/**
+ * Starts the recognition of faces
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ */
+async function startCamera() {
   if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(async (stream) => {
@@ -75,4 +134,106 @@ window.onload = async () => {
         throw error;
       });
   }
+};
+
+/**
+ * Tries to recognize the current camera image
+ *
+ * @author mauricio.araldi
+ * @since 0.2.0
+ * 
+ * @return {String} Name of the recognized user
+ */
+async function recognizeFace() {
+  setAppStatus(STATUS_RECOGNIZING, 'Checking face...');
+
+  const tempCanvas = document.createElement('canvas');
+  tempCanvas.height = CANVAS_HEIGHT;
+  tempCanvas.width = CANVAS_WIDTH;
+
+  const tempCtx = tempCanvas.getContext('2d');
+  tempCtx.drawImage(video, 0, 0, tempCanvas.width, tempCanvas.height);
+
+  sendRecognitionRequest(tempCanvas.toDataURL()).then(
+    result => {
+      if (!result) {
+        return setAppStatus(STATUS_NOT_RECOGNIZED, 'User not recognized');
+      }
+
+      setAppStatus(STATUS_RECOGNIZED, `Hello ${result}`);
+    },
+    error => {
+      throw error;
+    }
+  );
+}
+
+/* App initialization */
+window.onload = async () => {
+  const goToNewUserButton = document.querySelector('#go-to-create-user');
+  const createNewUserForm = document.querySelector('#create-user-form');
+
+  setAppStatus(STATUS_LOADING, 'Loading... It may take a while...');
+
+  changePage('login');
+
+  await startCamera();
+
+  setTimeout(() => {
+    recognizeFace();
+  }, 1000);
+
+  if (getAppStatus === STATUS_LOADING) {
+    setAppStatus('');
+  }
+
+  /* Add events */
+  goToNewUserButton.addEventListener('click', () => {
+    changePage('new-user');
+
+    const canvas = document.querySelector('#new-user-photo');
+    const ctx = canvas.getContext('2d');
+
+    canvas.height = CANVAS_HEIGHT;
+    canvas.width = CANVAS_WIDTH;
+
+    videoToCanvasRenderer = setInterval(() => {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    }, 100);
+  });
+
+  createNewUserForm.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+
+    const canvas = document.querySelector('#new-user-photo');
+    const userName = document.querySelector('#name').value;
+    const userImage = canvas.toDataURL();
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    fetch('/newuser', {
+      headers,
+      body: JSON.stringify({
+        image: userImage,
+        name: userName
+      }),
+      method: 'POST'
+    })
+      .then(data => data.json())
+      .then(response => {
+        if (!response.user) {
+          return setAppStatus(STATUS_USER_NOT_CREATED, 'Error. User not created.');
+        }
+
+        setAppStatus(USER_CREATED, 'User created');
+
+        changePage('login');
+
+        setTimeout(() => {
+          recognizeFace();
+        }, 1000);
+      })
+      .catch(error => reject(error));
+  });
 };
